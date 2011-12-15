8de8ecf26488a2a07c9372fb1547764def0d56b3
Way Spurr-Chen <way@heyway.org>
2011-12-15 15:00:50 -0500

    Added MySQL constants and Economy/AYE module booleans

--- src/me/theheyway/GPP/Constants.java
+++ src/me/theheyway/GPP/Constants.java
@@ -1,15 +1,40 @@
 package me.theheyway.GPP;
 
-import java.util.HashMap;
-
 public class Constants {
 	
 	public static final String SQLITE_DB_PATH = "plugins/GPP/db.sqlite";
+	public static final String PLUGIN_DIR = "plugins/GPP";
+	public static final String MASTER_CONFIG = "plugins/GPP/config.yml";
+	
+	public static boolean VERBOSE;
+	
+	public static String MYSQL_HOSTNAME;
+	public static int MYSQL_PORT;
+	public static String MYSQL_USERNAME;
+	public static String MYSQL_PASSWORD;
+	public static String MYSQL_DBNAME;
+	
+	public static boolean AREYOUEXPERIENCED_ENABLED;
+	public static boolean ECONOMY_ENABLED;
+	
 	
 	public GPP plugin;
 	
 	public Constants(GPP plugin) {
 		this.plugin = plugin;
+		
+		VERBOSE = plugin.getConfig().getBoolean("General.Verbose", false);
+		
+		MYSQL_HOSTNAME = plugin.getConfig().getString("MySQL.hostname");
+		MYSQL_PORT = plugin.getConfig().getInt("MySQL.port");
+		MYSQL_USERNAME = plugin.getConfig().getString("MySQL.username");
+		MYSQL_PASSWORD = plugin.getConfig().getString("MySQL.password");
+		MYSQL_DBNAME = plugin.getConfig().getString("MySQL.db_name");
+		GPP.logger.info("MYSQL_HOSTNAME: " + MYSQL_HOSTNAME);
+		GPP.logger.info("MYSQL_PORT: " + MYSQL_PORT);
+		GPP.logger.info("MYSQL_USERNAME: " + MYSQL_USERNAME);
+		GPP.logger.info("MYSQL_PASSWORD: " + MYSQL_PASSWORD);
+		GPP.logger.info("MYSQL_DBNAME: " + MYSQL_DBNAME);
 		
 	}
 
